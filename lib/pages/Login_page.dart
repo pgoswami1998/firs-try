a6ebd7176a3aa903c5d7baa7ec65b4ca325be7db
pradhyuman <pradhyumangoswami1998@gmail.com>
2021-08-20 17:06:55 +0530

    new

--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Material(
-    child:Column(
-      children: [
-        Image.asset("assets/images/login_images.png"),
-      ],
-    )
-
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_images.png", fit: BoxFit.cover),
+            Text("welcome",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                )),
+          ],
+        ));
   }
 }
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: LoginPage(),
       themeMode: ThemeMode.dark,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(
